19b182ff477a81af5fec5d15ff061e84cec61b90
Ng Jun Hao <jun.hao.ng.93@gmail.com>
2016-10-02 19:35:23 +0800

    Added profile, main menu and login page

---
+++ 2d/Assets/CameraManger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CameraManger : MonoBehaviour {
+
+	// Use this for initialization
+	Camera[] cams;
+	InputField usernameField, passwordField;
+	Canvas[] canvas;
+
+	void Start(){
+		cams = Camera.allCameras;
+		usernameField = GameObject.FindWithTag("UsernameField").GetComponent<InputField>();
+		canvas = GetComponentsInParent<Canvas> ();
+	}
+
+	public void loginCamera(){
+		Debug.Log (usernameField.text);
+		if (string.Compare (usernameField.text, "test") == 0) {
+			cams [1].gameObject.SetActive (false);
+			cams [2].gameObject.SetActive (true);
+			cams [3].gameObject.SetActive (false);
+
+		}
+
+		
+	}
+
+	public void storyCamera(){
+		cams [1].gameObject.SetActive (true);
+		cams [2].gameObject.SetActive (false);
+		cams [3].gameObject.SetActive (false);
+	}
+}
